5feb60a39c4ab8514c6660878281be8394aef069
vkammili5 <vijaylakshmikammili@gmail.com>
2022-07-23 20:09:48 +0100

    Add FlightData.json file in Data Folder, add Flight class in Model folder, add GetFlightData method in HolidaySearch class

---
+++ HolidaySearch/Data/OnTheBeachDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HolidaySearch.Data
+{
+    public class OnTheBeachDbContext 
+    {
+        private static OnTheBeachDbContext dbContext;
+        
+        public OnTheBeachDbContext()
+        {
+            Flights = new List<Flight>();
+            
+            LoadJsonData();
+        }
+
+        private void LoadJsonData()
+        {
+            StreamReader streamReader;
+            var path = Path.Combine(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location), "Data\\");
+
+            using (streamReader = new StreamReader(String.Format("{0}{1}", path, "FlightData.json")))
+            {
+                string flightData = streamReader.ReadToEnd();
+                Flights.AddRange(JsonConvert.DeserializeObject<List<Flight>>(flightData));
+
+            }
+        }
+        public List<Flight> Flights { get; set; }
+
+    }
+}
