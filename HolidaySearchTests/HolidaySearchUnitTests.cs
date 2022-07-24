067a42b345595c163ae95403463aea7b434b45c6
vkammili5 <vijaylakshmikammili@gmail.com>
2022-07-24 16:39:21 +0100

    add more test cases

--- HolidaySearchTests/HolidaySearchUnitTests.cs
+++ HolidaySearchTests/HolidaySearchUnitTests.cs
@@ -61,7 +61,33 @@ namespace HolidaySearchTests
             Assert.IsNull(results.Hotels, "Holiday search hotel results are not null");
         }
 
+        [Test]
+        public void IsHolidaySearch_Results_IsNotEmpty_Return_True()
+        {
+            holidaySearch = new HolidaySearch("MAN", "AGP", "2023-07-01", "7");
 
+            var results = holidaySearch.Results();
 
+            Assert.IsNotNull(results, "Holiday Search results are empty");
+        }
+
+        [Test]
+        public void HolidaySearch_Results_Test1_Multiple_Assertions()
+        {
+            holidaySearch = new HolidaySearch("MAN", "AGP", "2023-07-01", "7");
+
+            var results = holidaySearch.Results();
+
+            Assert.IsNotNull(results, "Holiday search results are null for test 1");
+
+            Assert.IsNotNull(results.Flights, "Holiday search flight results are empty for test 1");
+
+            Assert.IsNotNull(results.Hotels, "Holiday search hotel results are empty for test 1");
+
+            Assert.NotZero(results.TotalPrice, "Holiday search results total price is 0 for test 1");
+
+            Assert.IsTrue(results.Flights.Id == 2, "Flight Id is not 6 for test 1");
+            Assert.IsTrue(results.Hotels.Id == 9, "Hotel Id is not 5 for test 1");
+        }
     }
 }
